f7da799df4024162094976c04c6f7001ef9e6f1b
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-10-23 06:26:42 +0300

    add routings : / , /posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+// import { Container } from '@material-us/core';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import NavBar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={() => <Redirect to="/posts" />} />
+        <Route exact path="/posts" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
