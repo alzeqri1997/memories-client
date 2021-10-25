bd115221b4a9633c45676df19417b0ed95c35aa9
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-10-25 19:29:48 +0300

    add Pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,1 +1,35 @@
+import React, { useEffect } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
 import {Pagination, PaginationItem} from '@material-ui/lab'
+import { Link } from 'react-router-dom';
+
+import { getPosts } from '../actions/posts';
+import useStyles from './styles';
+
+const Paginate = ({ page }) => {
+    const {numberOfPages} = useSelector((state) => state.postReducer)
+    const dispatch = useDispatch();
+
+    const classes = useStyles();
+
+    useEffect(() => {
+        if (page) {
+            dispatch(getPosts(page))
+        }
+    }, [dispatch, page]);
+
+    return (
+        <Pagination
+            classes={{ us: classes.ul }}
+            count={numberOfPages}
+            page={Number(page)}
+            variant="outlined"
+            color="primary"
+            renderItem={(item) => {
+                <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
+            }}
+        />
+    )
+}
+
+export default Paginate;
