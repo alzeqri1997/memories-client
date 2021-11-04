a8f287d43a39df21967c163617985d4a5e53ad73
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-04 23:16:04 +0300

    add auth route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 // import { Container } from '@material-us/core';
 import React from "react";
+import { Container } from "@material-ui/core";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 // import NavBar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 import Home from "./components/Home/Home";
 import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
-      <Switch>
-        <Route exact path="/" component={() => <Redirect to="/posts" />} />
-        <Route exact path="/posts" component={Home} />
-        <Route exact path="/posts/search" component={Home} />
-        <Route exact path="/posts/:id" component={PostDetails} />
-      </Switch>
+      <Container maxWidth="xl">
+        {/* <NavBar /> */}
+        <Switch>
+          <Route exact path="/" component={() => <Redirect to="/posts" />} />
+          <Route exact path="/posts" component={Home} />
+          <Route exact path="/posts/search" component={Home} />
+          <Route exact path="/posts/:id" component={PostDetails} />
+          <Route
+            exact
+            path="/auth"
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
+        </Switch>
+      </Container>
     </BrowserRouter>
   );
 };
